35c7eb4ce63e66c06072ec5e3a88fb66920edd17
Sarmiento <genesissarmiento17@augustana.edu>
2020-03-07 17:17:41 -0600

    created AboutActivity and made aboutButton direct to it

--- app/src/main/java/com/example/puffinradio/MainActivity.java
+++ app/src/main/java/com/example/puffinradio/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.puffinradio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
+    private Button aboutButton;
+    private Button leaderBoardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutButton = (Button)findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), AboutActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
