df2d0381662f88443e6afc51dc402e52e58a7959
Maegan Patterson <maeganpatterson16@augustana.edu>
2020-03-30 23:45:41 -0500

    started testing how to use soundpool to make morse code sounds

--- app/src/main/java/com/example/puffinradio/GameActivity.java
+++ app/src/main/java/com/example/puffinradio/GameActivity.java
@@ -3,7 +3,12 @@ package com.example.puffinradio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,11 +25,28 @@ public class GameActivity extends AppCompatActivity {
     int time = 0;
     boolean hasStatic = false;
     String callsign = "";
+    SoundPool soundPool;
+    int dash, dot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+
+        } else {
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        }
 
 
         settingsButton = (Button)findViewById(R.id.settingsButton);
@@ -35,7 +57,89 @@ public class GameActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        dash = soundPool.load(this, R.raw.dash, 1);
+        dot = soundPool.load(this, R.raw.dot, 1);
+        sendCallSignButton = (Button)findViewById(R.id.sendCallSignButton);
+        // Just using the button for testing purposes for now; "hello world"
+        sendCallSignButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                helloWorld();
+            }
+        });
+
     }
 
+    public void rest(int length) {
+        try {
+            Thread.sleep(length);
+        } catch(InterruptedException e) {
+            Log.e("Error", "rest: Interrupted Exception");
+        }
+    }
+
+    public void dot() {
+        try {
+            soundPool.play(dot, 1, 1, 0, 0, 3);
+            Thread.sleep(666);
+        } catch(InterruptedException e) {
+            Log.e("Error", "dot: Interrupted Exception");
+        }
+    }
+
+    public void dash() {
+        try {
+            soundPool.play(dash, 1, 1, 0, 0, 1);
+            Thread.sleep(1333);
+        } catch(InterruptedException e) {
+            Log.e("Error", "dash: Interrupted Exception");
+        }
+    }
+
+    // Test: spell out "hello world" in Morse code
+    public void helloWorld() {
+        dot();
+        dot();
+        dot();
+        dot();
+        rest(666);
+        dot();
+        rest(666);
+        dot();
+        dash();
+        dot();
+        dot();
+        rest(666);
+        dot();
+        dash();
+        dot();
+        dot();
+        rest(666);
+        dash();
+        dash();
+        dash();
+        rest(2000);
+        dot();
+        dash();
+        dash();
+        rest(666);
+        dash();
+        dash();
+        dash();
+        rest(666);
+        dot();
+        dash();
+        dot();
+        rest(666);
+        dot();
+        dash();
+        dot();
+        dot();
+        rest(666);
+        dash();
+        dot();
+        dot();
+    }
 
 }
