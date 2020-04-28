305ea4cea417a6e7dec17a533c01d8eec425d816
DanTenorio <juantenorio16@augustana.edu>
2020-04-28 10:15:33 -0500

    Commented on settings

--- app/src/main/java/com/example/puffinradio/SettingsActivity.java
+++ app/src/main/java/com/example/puffinradio/SettingsActivity.java
@@ -10,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    /**
+     * This creates the activity and creates the settings
+     * @param savedInstanceState returns the last thing the user inputs
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +29,20 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * This sets up the Settings and its fragments using preferences
+     */
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
+
+    /**
+     * This opens the game activity after the user hits play
+     * @param view The Button
+     */
     public void openGameActivity(View view){
         startActivity(new Intent(SettingsActivity.this, GameActivity.class));
     }
