16f38660f47b2c5c9fe703a02c28ed2506e4c123
Maegan Patterson <maeganpatterson16@augustana.edu>
2020-04-29 22:42:50 -0500

    started implementing multiple difficulties

--- app/src/main/java/com/example/puffinradio/SettingsActivity.java
+++ app/src/main/java/com/example/puffinradio/SettingsActivity.java
@@ -2,14 +2,21 @@ package com.example.puffinradio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    boolean competitive;
+
     /**
      * This creates the activity and creates the settings
      * @param savedInstanceState returns the last thing the user inputs
@@ -17,6 +24,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
@@ -26,6 +34,29 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        Intent intent = this.getIntent();
+        competitive = intent.getBooleanExtra("competitive", false);
+        Spinner difficulty = findViewById(R.id.difficultySpinner);
+        TextView diffTextView = findViewById(R.id.difficultyText);
+        if(competitive) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.difficulties, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            difficulty.setAdapter(adapter);
+            difficulty.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> parent, View view,
+                                           int pos, long id) {
+                    parent.getItemAtPosition(pos);
+                }
+
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+
+            });
+        } else {
+            difficulty.setVisibility(View.GONE);
+            diffTextView.setVisibility(View.GONE);
+        }
+
     }
 
 
