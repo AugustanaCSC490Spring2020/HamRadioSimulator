2a24fc3d550a830af60575d6f5b417c65091c4f7
Maegan Patterson <maeganpatterson16@augustana.edu>
2020-05-13 15:14:55 -0500

    warn user and don't change wpm if they enter a value not between 5 and 60

--- app/src/main/java/com/example/puffinradio/SettingsActivity.java
+++ app/src/main/java/com/example/puffinradio/SettingsActivity.java
@@ -2,16 +2,23 @@ package com.example.puffinradio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.text.InputType;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
@@ -61,6 +68,13 @@ public class SettingsActivity extends AppCompatActivity {
             diffTextView.setVisibility(View.GONE);
         }
         gameSettings = new GameSettings(PreferenceManager.getDefaultSharedPreferences(this));
+        FrameLayout l = findViewById(R.id.settings);
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
@@ -71,7 +85,21 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
-            androidx.preference.EditTextPreference CWpref = getPreferenceManager().findPreference("WPM");
+            final androidx.preference.EditTextPreference CWpref = getPreferenceManager().findPreference("WPM");
+            CWpref.setDialogMessage("Speed must be between 5-60 WPM");
+            CWpref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    String s = newValue.toString();
+                    if(Integer.parseInt(s) < 5 || Integer.parseInt(s) > 60) {
+                        Toast toast = Toast.makeText(getContext(), "Speed must be between 5-60 WPM", Toast.LENGTH_SHORT);
+                        toast.show();
+                        return false;
+                    }
+                    return true;
+                }
+            });
+
             CWpref.setOnBindEditTextListener(new androidx.preference.EditTextPreference.OnBindEditTextListener(){
                 @Override
                 public void onBindEditText(@NonNull EditText editText) {
